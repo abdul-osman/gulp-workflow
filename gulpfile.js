010af62b80c72652de112044b16774c69aaa59e9
Abdul Osman <abdulosman@Abduls-MacBook-Pro.local>
2017-08-26 00:46:23 +0100

    gulp-sass and gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const prefix = require('gulp-autoprefixer');
+const reload = browserSync.reload;
 
 
+
+const SOURCEPATHS = {
+  sassSource: 'src/scss/*.scss'
+}
+const APPPATH = {
+  root: 'app/',
+  css: 'app/css',
+  js: 'app/js',
+  scss: 'src/scss'
+}
 
 
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default');
+gulp.task('serve', ['sass'], function(){
+  browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '*.js'], {
+    server: {
+      baseDir: APPPATH.root
+    }
+  })
+});
+
+gulp.task('watch', function () {
+    gulp.watch(APPPATH.scss + '/*.scss', ['sass']);
+});
+
+
+gulp.task('default', ['watch', 'serve']);
